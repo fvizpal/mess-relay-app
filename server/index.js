c1db40cede0a875151c6bd053c420b5b43634b27
fvizpal <vishalp9966@gmail.com>
2023-11-08 14:12:53 +0530

    setting server

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import helmet from "helmet";
 
+import authRoutes from "./routes/auth.js";
+
+/* CONFIGURATIONS */
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -13,3 +17,18 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 
 app.use(cors());
+
+/* Routes */
+app.use("/auth", authRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
+    })
+    .catch((error) => console.log(`${error} \n did not connect`));
---
+++ server/models/User.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        min: 2,
+        max: 50,
+    },
+});
+
+const User = mongoose.model("User", UserSchema);
+export default User;
