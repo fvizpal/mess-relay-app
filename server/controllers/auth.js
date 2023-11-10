dd3774f59b14be82bb0873c57688d36cd94a5943
fvizpal <vishalp9966@gmail.com>
2023-11-10 07:34:24 +0530

    added register controllers

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,30 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
+/* REGISTER */
+export const register = async (req, res) => {
+    try {
+        const { firstName, lastName, email, password, role, hostel } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password: passwordHash,
+            role,
+            hostel,
+        });
+
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
 /* LOGGING IN */
 export const login = async (req, res) => {
     try {
