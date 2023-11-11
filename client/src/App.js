85295a1ba132907f108db1d71b5cb06c7393e827
fvizpal <vishalp9966@gmail.com>
2023-11-11 16:49:36 +0530

    updated route

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+// import { useMemo } from "react";
+
 import HomePage from "scenes/HomePage";
 import LoginPage from "scenes/LoginPage";
 
 function App() {
+    const isAuth = Boolean(useSelector((state) => state.token));
     return (
         <div className="App">
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<LoginPage />} />
-                    <Route path="/home" element={<HomePage />} />
+                    <Route
+                        path="/home"
+                        element={isAuth ? <HomePage /> : <Navigate to="/" />}
+                    />
                 </Routes>
             </BrowserRouter>
         </div>
