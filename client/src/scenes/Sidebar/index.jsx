abb6b6403e2ed4cb3f449b2a485202adb8310343
fvizpal <vishalp9966@gmail.com>
2023-11-13 08:55:24 +0530

    adding tabs

--- client/src/scenes/Sidebar/index.jsx
+++ client/src/scenes/Sidebar/index.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Box, Typography, useMediaQuery } from "@mui/material";
 
 const Sidebar = () => {
-    return <div>Sidebar</div>;
+    const user = useSelector((state) => state.user);
+    const isAdmin = user.role === "admin";
+    const isDesktop = useMediaQuery("(min-width:600px)");
+    const drawerWidth = "250px";
+
+    return (
+        <Box>
+            {isAdmin ? (
+                <>
+                    <Typography>Sidebar shall be here ...</Typography>
+                    <Typography>
+                        No kidding imma really put the sidebar here, dont
+                        believe ? come back after a short while.
+                    </Typography>
+                </>
+            ) : (
+                <>
+                    <Typography>Sidebar shall be here ...</Typography>
+                    <Typography>
+                        No kidding imma really put the sidebar here, dont
+                        believe ? come back after a short while.
+                    </Typography>
+                </>
+            )}
+        </Box>
+    );
 };
 
 export default Sidebar;
