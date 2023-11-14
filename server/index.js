5b7af11f5ebae66947a90d64700df247a19369cf
fvizpal <vishalp9966@gmail.com>
2023-11-14 15:40:19 +0530

    restructuring

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import mongoose from "mongoose";
-import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import morgan from "morgan";
@@ -10,69 +9,46 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/auth.js";
-
-//importing models
-import Menu from "./models/menu.js";
-import MenuRoutes from "./routes/menuRoutes.js"
+import { register } from "./controllers/auth.js";
 
 import { verifyToken } from "./middleware/auth.js";
-import { connect } from "http2";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// dotenv config
 dotenv.config();
 const app = express();
-//MiddleWares
-app.use(express.json());  
-app.use(express.urlencoded({extended:false}));
-app.use(MenuRoutes);       // for using routes of Menu
-
+app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
- 
+
+// FILE STORAGE
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
+const upload = multer({ storage });
+
+//Routes with files
+app.post("/auth/register", upload.single("picture"), register);
+
 /* Routes */
 app.use("/auth", authRoutes);
 
-
-// MONGOOSE SETUP (firstly connect the database then fire the server)
+// MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>{
-    app.listen(PORT,  ()=>{
-        console.log(`server running on port ${PORT}`);
-    }) 
-})
-.catch((err)=> console.log(err));
-
-
-
-// ROUTES 
-// get req
-app.get("/", (req,res)=>{
-    res.send("This is HomePage");
-})
-
-app.get("/login", (req,res)=>{
-    res.send("This is LoginPage");
-})
-
-app.get("/register", (req,res)=>{
-    res.send("This is registerPage");
-})
-
-app.get("/contact", (req,res)=>{
-    res.send("This is Contact Us Page");
-})
-
-app.get("/admin", (req,res)=>{
-    res.send("This is Admin Portal");
-})
-
-
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
+    })
+    .catch((error) => console.log(`${error} \ndid not connect`));
